c8b16d8dbaaa57a4ce2d4ca84b2f229806ad01d5
hanchenhui <hanchenhui@leimingtech.com>
2020-06-16 11:48:47 +0800

    init

---
+++ shardingdb/src/main/java/com/hch/shadingdb/MybootApplication.java
@@ -0,0 +1,13 @@
+package com.hch.shadingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybootApplication.class, args);
+    }
+
+}
---
+++ simpleboot/src/main/java/com/hch/simpleboot/SimpleBootApplication.java
@@ -0,0 +1,13 @@
+package com.hch.simpleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBootApplication.class, args);
+    }
+
+}
